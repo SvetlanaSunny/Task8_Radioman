6f7f0ae019ece046183b9e2e872c3f2398ea104d
Светлана Чернова <lana_19.87@mail.ru>
2022-04-22 15:57:57 +0600

    Task10 first commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,8 +1,18 @@
 package ru.netology;
 
 public class Radio {
-  int radioStationNumber;
-  int soundVolume;
+  protected int radioStationNumber; /* номер радиостанции*/
+  protected int soundVolume; /* громкость звука*/
+  private int radioAmount = 10; /* кол-во радиостанций*/
+
+   public Radio(){
+
+  }
+
+  public Radio(int radioAmount){
+    this.radioAmount = radioAmount;
+  }
+
 
   public int getRadioStationNumber() {
     return radioStationNumber;
@@ -13,15 +23,17 @@ public class Radio {
     if (RadioStationNumber < 0) {
       return;
     }
-    if (RadioStationNumber > 9) {
+    if (RadioStationNumber > (radioAmount-1)) {
       return;
     }
     this.radioStationNumber = RadioStationNumber;
   }
 
   public void nextRadioStationNumber() {
-
-    if (radioStationNumber == 9) {
+     if (radioStationNumber >= radioAmount){
+        this.radioStationNumber = (radioAmount-1);
+     }
+    if (radioStationNumber == (radioAmount-1)) {
       radioStationNumber = 0;
     } else {
       radioStationNumber++;
@@ -29,9 +41,11 @@ public class Radio {
   }
 
   public void prevRadioStationNumber() {
-
+    if (radioStationNumber >= radioAmount){
+      this.radioStationNumber = (radioAmount-1);
+    }
     if (radioStationNumber == 0) {
-      radioStationNumber = 9;
+      radioStationNumber = (radioAmount-1);
     } else {
       radioStationNumber--;
     }
@@ -47,21 +61,21 @@ public class Radio {
     if (soundVolume < 0) {
       return;
     }
-    if (soundVolume > 10) {
+    if (soundVolume > 100) {
       return;
     }
     this.soundVolume = soundVolume;
   }
 
   public void increaseVolume() {
-    if (soundVolume < 10) {
+    if (soundVolume < 100) {
       soundVolume++;
     }
     this.soundVolume = soundVolume;
   }
 
   public void decreaseVolume() {
-    if (soundVolume >= 10) {
+    if (soundVolume > 0) {
       soundVolume--;
     }
     this.soundVolume = soundVolume;
